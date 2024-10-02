ce4c555fc8dee12652a0cb53ab6d61f9cebedba8
Yusha <faizan.mashuq@gmail.com>
2024-10-02 23:09:34 +0600

    grid  added"

--- src/components/Recipie/Recipie.jsx
+++ src/components/Recipie/Recipie.jsx
@@ -1,11 +1,14 @@
 import PropTypes from 'prop-types';
+import './Recipie.css'
 
 const Recipie = ({recipie}) => {
-    const {recipe_id, recipe_name, ingredients, preparing_time,calories} = recipie
+    const {image, recipe_id, recipe_name, ingredients, preparing_time,calories} = recipie
     return (
-        <div>
+        <div className='single-recipie'>
+            <img className='recipie-image' src={image} />
             <h3>ID: {recipe_id}</h3>
             <h3>Name: {recipe_name}</h3>
+        
             <div>
                 <h4>Ingredients: {ingredients.length}</h4>
                 {/* {
@@ -20,6 +23,9 @@ const Recipie = ({recipie}) => {
             </div>
             <p>Preparing Time: {preparing_time}</p>
             <p>Calories: {calories}</p>
+            <span>
+                <button className='cook-btn'>Want to cook</button>
+            </span>
         </div>
     );
 };
--- src/components/Recipies/Recipies.jsx
+++ src/components/Recipies/Recipies.jsx
@@ -1,30 +1,41 @@
 import PropTypes from 'prop-types';
 import { useEffect, useState } from 'react';
 import Recipie from '../Recipie/Recipie';
+import './Recipies.css'
 
 const Recipies = () => {
     const [recipies, setRecipies] = useState([])
-    useEffect(() =>{
+    useEffect(() => {
         fetch('test.json')
-        .then(res => res.json())
-        .then(data => setRecipies(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setRecipies(data))
+    }, [])
     return (
         <div>
-            <h1>Total Recipies: {recipies.length}</h1> 
+            <h1>Total Recipies: {recipies.length}</h1>
             {/* <h1>Total Recipies: {Array.isArray(recipies) ? recipies.length : 0}</h1> */}
-           {
-             recipies.map(recipie => <Recipie key={recipie.recipe_id}
-             recipie={recipie}></Recipie>)
-           }
-            
+            <div className='body-sub-container'>
+                <div className='recipies-container'>
+                    {
+                        recipies.map(recipie => <Recipie key={recipie.recipe_id}
+                            recipie={recipie}></Recipie>)
+                    }
+                </div>
+
+                <div>
+                    <span>
+                        <h1>Want to Cook</h1>
+                    </span>
+                </div>
+            </div>
+
         </div>
     );
 };
 
 Recipies.propTypes = {
     recipies: PropTypes.array
-    
+
 }
 
 export default Recipies;
