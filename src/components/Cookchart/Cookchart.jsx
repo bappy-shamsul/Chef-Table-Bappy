cf96e98255b780810b8841cd55a2a95ed8d08290
Yusha <faizan.mashuq@gmail.com>
2024-10-07 00:25:38 +0600

    table added

--- src/components/Cookchart/Cookchart.jsx
+++ src/components/Cookchart/Cookchart.jsx
@@ -1,10 +1,35 @@
 import PropTypes from 'prop-types';
+import '../Cookchart/Cookchart.css'
 
-const Cookchart = ({cookchart}) => {
+const Cookchart = ({cooklist}) => {
+    const {recipe_name, preparing_time, calories, recipe_id} = cooklist;
     return (
         <div>
-            <h2>{cookchart.recipe_name}</h2>
+
+            <table border="1" cellPadding="10" cellSpacing="0">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Time</th>
+                        <th>Calories</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                     <tr >
+                         <td>{recipe_id }</td>
+                         <td>{recipe_name }</td>
+                         <td>{preparing_time}</td>
+                         <td>{calories}</td>
+                         <button className='btn-preparing'>Preparing</button>
+                     </tr>
+                    }
+                </tbody>
+                
+            </table>
         </div>
+       
     );
 };
 
--- src/components/Cooklists/Cooklists.jsx
+++ src/components/Cooklists/Cooklists.jsx
@@ -1,9 +1,14 @@
 import PropTypes from 'prop-types';
+import Cookchart from '../Cookchart/Cookchart';
 
 const Cooklists = ({recipies}) => {
     return (
         <div>
            <h2> Want to Cook: {recipies.length}</h2>
+           {
+            recipies.map(cooklist => <Cookchart key={cooklist.recipe_id} 
+                cooklist={cooklist}></Cookchart>)
+           }
         </div>
     );
 };
